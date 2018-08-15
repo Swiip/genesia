dae27a014502b70ee3ca2cbff02783d03f4312af
swiip <swiip@MacBook-Pro-de-swiip.local>
2018-08-15 18:20:38 +0200

    hello world client

---
+++ client/home.js
@@ -0,0 +1,3 @@
+import { component, withMarkup, html } from "/node_modules/compo/index.js";
+
+component("gen-home", withMarkup(() => html`<h1>Genesia</h1>`));
--- server/index.js
+++ server/index.js
@@ -6,9 +6,10 @@ const bodyParser = require("body-parser");
 const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
 const { makeExecutableSchema } = require("graphql-tools");
 
-const typeDefs = fs
-  .readFileSync(path.resolve(__dirname, "./schema.graphql"))
-  .toString();
+const schemaPath = path.resolve(__dirname, "./schema.graphql");
+const staticPath = path.resolve(__dirname, "../client");
+
+const typeDefs = fs.readFileSync(schemaPath).toString();
 
 const resolvers = {
   Query: {
@@ -25,6 +26,6 @@ const app = express();
 
 app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
 app.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
-app.use(express.static("../client/public"));
+app.use(express.static(staticPath));
 
 app.listen(3000, () => console.log("Genesia server listening on port 3000!"));
