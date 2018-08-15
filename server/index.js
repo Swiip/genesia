d45887af921e8700c7d5f4dbaa6baeb5c18d3f82
swiip <swiip@MacBook-Pro-de-swiip.local>
2018-08-15 18:06:42 +0200

    add graphql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,30 @@
+const fs = require("fs");
+const path = require("path");
+
 const express = require("express");
+const bodyParser = require("body-parser");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const typeDefs = fs
+  .readFileSync(path.resolve(__dirname, "./schema.graphql"))
+  .toString();
+
+const resolvers = {
+  Query: {
+    players: () => [{ name: "Swiip" }]
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
 const app = express();
 
+app.use("/graphql", bodyParser.json(), graphqlExpress({ schema }));
+app.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 app.use(express.static("../client/public"));
 
 app.listen(3000, () => console.log("Genesia server listening on port 3000!"));
